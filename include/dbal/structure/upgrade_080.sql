c5f28156247b133bfcfaf5f7406ac56c65cb8575
Joshua <c.treyce@gmail.com>
2008-02-09 20:27:30 +0000

    UniAdmin: - Updated pclzip.lib.php to 2.6 - Added svn metadata on upgrade_080.sql (keywords,eol-style)

--- include/dbal/structure/upgrade_080.sql
+++ include/dbal/structure/upgrade_080.sql
@@ -1,7 +1,7 @@
 #
 # MySQL UniAdmin Upgrade File
 #
-# * $Id: upgrade_078.sql 24 2007-06-17 23:39:37Z Zanix $
+# * $Id$
 #
 # --------------------------------------------------------
 ### Alter uniadmin_config
