1baca25213d3a1a6c0adbebaffa65c3ee730f340
Calystos <Calystos@cd6b52ad-ba8a-21b6-d4c0-faacabb78645>
2009-10-05 21:49:05 +0000

    UniAdmin - added - options for new uniuploader features (upload all accounts, uniuploader auto-update, roster URL, forum URL, store password securely, close to systray).

--- trunk/include/dbal/structure/upgrade_079.sql
+++ trunk/include/dbal/structure/upgrade_079.sql
@@ -18,18 +18,24 @@ INSERT INTO `uniadmin_config` (`config_name`, `config_value`, `form_type`) VALUE
 ### Alter uniadmin_settings
 
 INSERT INTO `uniadmin_settings` (`set_name` , `set_value` , `enabled` , `section` , `form_type` ) VALUES
-	('USERAGENT','UniUploader 2.0 (UU 2.6.8 BETA 3; English)','0', 'options', 'text{250|50'),
+	('ROSTERURL', 'http://yourdomain.com/roster', '0', 'options', 'text{250|50'),
+	('UNIADMINURL', 'http://yourdomain.com/uniadmin', '0', 'options', 'text{250|50'),
+	('USERAGENT','UniUploader 2.0 (UU 2.6.9; English)','0', 'options', 'text{250|50'),
 	('UPERRPOPUP','1','0', 'options', 'radio{yes^1|no^0'),
 	('CLOSEAFUPD','0','0', 'options', 'radio{yes^1|no^0'),
 	('CLOSEAFLAU','0','0', 'options', 'radio{yes^1|no^0'),
 
 	('AUTOSYNCIN','0','0', 'updater', 'text{250|50'),
 	('UPDATESURL', 'http://www.wowroster.net/uniuploader_updater2/update.php', '0', 'updater', 'text{250|50'),
+	('UUAUTOUPDATERCHECK','0','0', 'updater', 'radio{yes^1|no^0'),
 
+	('STOREPWSECURE','1','0', 'advanced', 'radio{yes^1|no^0'),
 	('USEAPPDATA','0','0', 'advanced', 'radio{yes^1|no^0'),
+	('CLOSETOSYSTRAY','0','0', 'advanced', 'radio{yes^1|no^0'),
 
 	('UPLOADSCREENSHOTS','1','0', 'settings', 'radio{yes^1|no^0'),
-	('UPLOADSVS','1','0', 'settings', 'radio{yes^1|no^0');
+	('UPLOADSVS','1','0', 'settings', 'radio{yes^1|no^0'),
+	('UPLOADALLACCOUNTS','1','0', 'settings', 'radio{yes^1|no^0');
 
 # --------------------------------------------------------
 ### Alter uniadmin_addons
