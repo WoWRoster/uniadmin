b7cc2e6987d1c5d5cdaa7d2ded5610fec1a37e84
Calystos <Calystos@cd6b52ad-ba8a-21b6-d4c0-faacabb78645>
2009-11-01 11:40:24 +0000

    UniAdmin Trunk - updated web_to_wow system to allow for optional standalone savedvariables.lua data file that is pure lua code, rather than the php files required php code thus making it easier to update and maintain/etc as you simply copy/paste whatever addons LUA data you want directly into the "SavedVariables.lua" file that you can create and store in the same place that the web_to_wow.php file is located at.

--- web_to_wow.php
+++ web_to_wow.php
@@ -18,7 +18,15 @@
 */
 
 // Enter the data you wish to output to WoW's SavedVariables.lua file here
-$svlua = "";
+if (file_exists("Savedvariables.lua")) {
+	// This allows you to create and use a local file on your website that is pure LUA code (rather than a PHP files code)
+	$svlua = include "Savedvariables.lua";
+	}
+else {  // File doesn't exist, use internal system instead
+	// If you use this option and have it all internal to the php file make sure to change the
+	// LUA code to php (eg instead of " you put \") for correct parsing
+	$svlua = ""
+	}
 
 // Now lets send the data to UniUploader for output to WoW's SavedVariables.lua file
 switch ($_REQUEST["OPERATION"])
